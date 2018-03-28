057b2417fe0b20c6954f98d9731badfb1d71591f
Abhijit Khanna <hindu_steve@yahoo.com>
2018-03-28 11:05:03 -0400

    Final solution to challenge

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -3,11 +3,34 @@ class CommentsController {
     this.$addCommentForm = $('.add-comment')
   }
 
-  init() {
+    init() {
     // kick off controller from here
+    this.addCommentFormListener();
   }
 
   addCommentFormListener() {
     // create comment form listener code here
-  }
+
+      var self = this;
+      this.$addCommentForm.on('click', 'input[type="submit"]', function(e){ //live event imageener
+        var imageId = parseInt($(this).parents('ul').data('id'));
+        var commentText = $(this).prev('input[type="text"]').val();
+
+        $(this).prev('input[type="text"]').val(""); // clear out user input
+        var newComment = new Comment(commentText, imageId);
+
+        e.preventDefault();
+
+        self.render(newComment);
+        newComment.findImage(); 
+
+      });
+    }
+
 }
+
+ CommentsController.prototype.render = function(commentObject) {
+
+    $(`ul[id="comments-${commentObject.id}"]`).append(commentObject.commentEl()); //was .appen(insertLi)
+
+  }
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,19 @@
 // create Comment class here
+
+
+function Comment(comment, imageId) {
+  this.id = imageId;
+  this.commentContent = comment;
+  Comment.all.push(this.commentContent);
+}
+
+ Comment.prototype.findImage = function() {
+ 	Image.all[this.id].comments.push(this.commentContent);
+ 	return Image.all[this.id];
+ }
+
+ Comment.prototype.commentEl = function() {
+ 	return `<li id="${Image.all[this.id].comments.length}">${this.commentContent}</li>`;
+ }
+
+  Comment.all = [];
